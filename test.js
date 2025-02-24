4932957c0ba0448cd4b83c4e715eed4e80b7618c
Kevin Chavez <mr.kev@me.com>
2025-02-24 17:01:41 -0600

    update cheerio

--- test.js
+++ test.js
@@ -7,50 +7,53 @@
 
 // B is longer
 function similarity(a, b) {
-    var i = 0;
-    while (i < b.length) {
-        if (a.charAt(i) != b.charAt(i)) return i;
-        i++;
-    }
-    return i;
+  var i = 0;
+  while (i < b.length) {
+    if (a.charAt(i) != b.charAt(i)) return i;
+    i++;
+  }
+  return i;
 }
 
 function StringSimilarity(inputs) {
-    var result = inputs.map(function(x) {
-        var i = 0, res = 0;
-        while (i <= x.length) {
-            res += similarity(x.slice(i), x);
-            i++;
-        }
-        return res;
+  var result = inputs
+    .map(function (x) {
+      var i = 0,
+        res = 0;
+      while (i <= x.length) {
+        res += similarity(x.slice(i), x);
+        i++;
+      }
+      return res;
     })
-    .map(function (x) {console.log(x);});
-    return result;
+    .map(function (x) {
+      console.log(x);
+    });
+  return result;
 }
-
 
 // StringSimilarity(['hello', 'aa']);
 
+const Plant = require("./Plant.js");
 
-var Plant = require('./Plant.js');
+const browser = new Plant(); // <- You can pass defaults for request.
+browser
+  .get("http://www.theuselessweb.com/")
 
-var browser = new Plant(); // <- You can pass defaults for request. 
-browser.get('http://www.theuselessweb.com/')
+  .then(function ($) {
+    console.log($("h3").text()); // USELESS
+    return browser.follow("a:nth-child(2)"); // <- Follows link with this selector
+  })
 
-.then(function($) {
-  console.log($('h3').text()); // USELESS
-  return browser.follow('a:nth-child(2)'); // <- Follows link with this selector
-})
+  .then(function ($) {
+    console.log($(".text-holster h1").text()); // SHUT UP& LOOK
+    return browser.get("http://www.theuselessweb.com/");
+  })
 
-.then(function($) {
-  console.log($('.text-holster h1').text()); // SHUT UP& LOOK
-  return browser.get('http://www.theuselessweb.com/');
-})
+  .then(function ($) {
+    console.log($("h3").text()); // USELESS
+  })
 
-.then(function($) {
-  console.log($('h3').text()); // USELESS
-})
-
-.catch(function(e) {
-  return console.trace(e);
-});
+  .catch(function (e) {
+    return console.trace(e);
+  });
