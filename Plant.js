ad82de0ff7adce0ba95b29bc28e9aa3f15e4bceb
Kevin Chavez <mr.kev@me.com>
2025-02-25 01:46:57 -0600

    remove dependency on coffee-script

--- Plant.js
+++ Plant.js
@@ -1,38 +1,41 @@
 "use strict";
-let rp         = require('./request_promise')
-let cheerio    = require('cheerio')
+let rp = require("./request_promise");
+let cheerio = require("cheerio");
 
 class Plant {
-
   constructor(options) {
     this.options = options;
     this.r = rp.defaults(this.options);
-    this.verbs = ['get', 'head', 'post', 'put', 'patch', 'del']
+    this.verbs = ["get", "head", "post", "put", "patch", "del"];
 
-    return this.$ = null;
+    return (this.$ = null);
   }
 
-  check_state(state) { switch (state) {
-    case 'doc':
-      if (!this.$) { throw new Error('No website has been loaded!') }
-  } }
+  check_state(state) {
+    switch (state) {
+      case "doc":
+        if (!this.$) {
+          throw new Error("No website has been loaded!");
+        }
+    }
+  }
 
   /** Follows a link in the current document. */
   follow(selector) {
-    this.check_state('doc')
-    let link = this.$(selector).attr('href')
-    if (!link) { throw new Error(`No link with selector ${selector}`) }
-    return (this.get(link))
+    this.check_state("doc");
+    let link = this.$(selector).attr("href");
+    if (!link) {
+      throw new Error(`No link with selector ${selector}`);
+    }
+    return this.get(link);
   }
 
   /** @return Promise to cheerio instance resolved by browser */
   get(opts) {
-    return this.r.get(opts)
-      .then(body => {
-        this.$ = cheerio.load(body)
-        return this.$
-      }
-    )
+    return this.r.get(opts).then((body) => {
+      this.$ = cheerio.load(body);
+      return this.$;
+    });
   }
 }
 
@@ -41,23 +44,24 @@ module.exports = Plant;
 if (require.main === module) {
   let browser = new Plant();
 
-  browser.get('http://www.theuselessweb.com/')
+  browser
+    .get("http://www.theuselessweb.com/")
 
-  .then(function($) {
-    console.log($('h3').text()); // USELESS
-    return browser.follow('a:nth-child(2)'); // <- Follows link with this selector
-  })
+    .then(function ($) {
+      console.log($("h3").text()); // USELESS
+      return browser.follow("a:nth-child(2)"); // <- Follows link with this selector
+    })
 
-  .then(function($) {
-    console.log($('.text-holster h1').text()); // SHUT UP& LOOK
-    return browser.get('http://www.theuselessweb.com/');
-  })
+    .then(function ($) {
+      console.log($(".text-holster h1").text()); // SHUT UP& LOOK
+      return browser.get("http://www.theuselessweb.com/");
+    })
 
-  .then(function($) {
-    console.log($('h3').text()); // USELESS
-  })
+    .then(function ($) {
+      console.log($("h3").text()); // USELESS
+    })
 
-  .catch(function(e) {
-    return console.trace(e);
-  });
+    .catch(function (e) {
+      return console.trace(e);
+    });
 }
--- index.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-/* global require, module */
-
-require('coffee-script/register');
-module.exports = require('./Plant.coffee');
